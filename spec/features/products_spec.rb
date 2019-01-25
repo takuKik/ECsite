22e95936e33fd883c4e497141a7d8661240d3fa5
Takuya KIkuchi <takuyakikuchi@TakuyanoMacBook-Air.local>
2019-01-25 12:38:04 +0900

    edit rspec

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
   describe "show details of a product" do
-    let(:product) { create(:product) }
+    given(:product) { create(:base_product) }
 
-    scenario "user visits a product details page" do
+    scenario "display each product user visits a product details page" do
       visit potepan_product_path(product.id)
       expect(page).to have_content product.name
       expect(page).to have_content product.display_price
