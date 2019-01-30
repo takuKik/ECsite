ad922b3e1d104dd5b7a1dd4ad16c9e90652a9101
Takuya KIkuchi <takuyakikuchi@TakuyanoMacBook-Air.local>
2019-01-30 17:33:22 +0900

    revise rspec

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,5 @@
 class Potepan::ProductsController < ApplicationController
   def show
-    @product = Spree::Product.includes(product_properties: :property).find(params[:id])
+    @product = Spree::Product.includes(product_properties: :property).friendly.find(params[:id])
   end
 end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
-  describe "show details of a product" do
-    given(:product) { create(:base_product) }
+  given(:product) { create(:base_product) }
 
-    scenario "display each product user visits a product details page" do
-      visit potepan_product_path(product.id)
-      expect(page).to have_content product.name
-      expect(page).to have_content product.display_price
-      expect(page).to have_content product.description
-    end
+  scenario "display each product detail using friendly_id" do
+    visit potepan_product_path(product)
+    expect(page).to have_title product.name
+    expect(page).to have_content product.name
+    expect(page).to have_content product.display_price
+    expect(page).to have_content product.description
+  end
+
+  scenario "display each product user visits a product details page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_title product.name
+    expect(page).to have_content product.name
+    expect(page).to have_content product.display_price
+    expect(page).to have_content product.description
   end
 end
