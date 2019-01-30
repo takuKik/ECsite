14187b1c75088b70ac5d648ff5045ba9789cb90f
Takuya KIkuchi <takuyakikuchi@TakuyanoMacBook-Air.local>
2019-01-31 02:02:18 +0900

    revise rspec

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,21 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
-  given(:product) { create(:base_product) }
+  describe "show details of a product" do
+    given(:product)           { create(:product) }
+    given(:property)          { create(:property) }
+    given!(:product_property) { create(:product_property, product: product, property: property) }
 
-  scenario "display each product detail using friendly_id" do
-    visit potepan_product_path(product)
-    expect(page).to have_title product.name
-    expect(page).to have_content product.name
-    expect(page).to have_content product.display_price
-    expect(page).to have_content product.description
-  end
-
-  scenario "display each product user visits a product details page" do
-    visit potepan_product_path(product.id)
-    expect(page).to have_title product.name
-    expect(page).to have_content product.name
-    expect(page).to have_content product.display_price
-    expect(page).to have_content product.description
+    it "display each product user visits a product details page" do
+      visit potepan_product_path(product.id)
+      expect(page).to have_title   product.name
+      expect(page).to have_content product.name
+      expect(page).to have_content product.display_price
+      expect(page).to have_content product.description
+      expect(page).to have_content product_property.value
+    end
   end
 end
