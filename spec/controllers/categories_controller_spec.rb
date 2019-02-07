111c8dce365c04b638a2ab12b79843d3f694cf75
Takuya KIkuchi <takuyakikuchi@TakuyanoMacBook-Air.local>
2019-02-07 11:39:13 +0900

    Create a test for the show action of CategoriesController

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::CategoriesController, type: :controller do
+  describe "#show" do
+    let(:taxonomy) { create(:taxonomy) }
+    let(:taxon) { taxonomy.root }
+    let(:child_taxon) { create(:taxon, taxonomy: taxonomy, parent: taxon) }
+    let(:product) { create(:product, taxons: [child_taxon]) }
 
+    before do
+      get :show, params: { id: child_taxon.id }
+    end
+
+    it "Success of the response" do
+      expect(response).to be_success
+    end
+
+    it "renders a correct page" do
+      expect(response).to render_template(:show)
+    end
+
+    it "correctly assigns data to @taxonomies" do
+      expect(assigns(:taxonomies)).to match_array(taxonomy)
+    end
+
+    it "correctly assigns data to @taxon" do
+      expect(assigns(:taxon)).to eq child_taxon
+    end
+
+    it "correctly assigns data to @products" do
+      expect(assigns(:products)).to match_array(product)
+    end
+  end
 end
