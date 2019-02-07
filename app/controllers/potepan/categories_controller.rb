954a5d7daf79dae8728b518d966ac699df102480
Takuya KIkuchi <takuyakikuchi@TakuyanoMacBook-Air.local>
2019-02-07 11:21:25 +0900

    show Product list

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,5 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.includes(:taxons)
+    @products = Spree::Product.in_taxon(@taxon).includes(master: [:images, :default_price])
   end
 end
