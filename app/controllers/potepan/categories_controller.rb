84db7cb4bfe482bb29349df4e509a1d26261a413
Takuya KIkuchi <takuyakikuchi@TakuyanoMacBook-Air.local>
2019-02-13 19:01:09 +0900

    fix categories_controller

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,7 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @taxonomies = Spree::Taxonomy.includes(:taxons)
+    @taxonomies = Spree::Taxonomy.includes(:root)
     @products = Spree::Product.in_taxon(@taxon).includes(master: [:images, :default_price])
   end
 end
