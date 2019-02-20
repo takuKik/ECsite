d3eef3c349de1f6680d32f631c4826b7bf398911
Takuya KIkuchi <takuyakikuchi@TakuyanoMacBook-Air.local>
2019-02-20 11:11:35 +0900

    Add @related_products to products_controller

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,6 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.includes(product_properties: :property).find(params[:id])
+    @related_products = Spree::Product.in_taxons(@product.taxons).includes(master: [:images, :default_price]).where.not(id: @product.id).distinct.limit(4)
   end
 end
