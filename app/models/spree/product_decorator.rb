3cd3ae6b6f039b4c3fc13a78efb1cf312aa57145
Takuya KIkuchi <ta.ku.ya7617@gmail.com>
2019-03-01 12:24:46 +0900

    Changed model expansion method from class_eval to prepend

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,9 @@
-Spree::Product.class_eval do
-  scope :related_products, -> (product) do
-    in_taxons(product.taxons).includes(master: [:images, :default_price]).where.not(id: product.id).distinct
+module Spree::ProductDecorator
+  def self.prepended(base)
+    base.scope :related_products, -> (product) do
+      in_taxons(product.taxons).includes(master: [:images, :default_price]).where.not(id: product.id).distinct
+    end
   end
+
+  Spree::Product.prepend self
 end
